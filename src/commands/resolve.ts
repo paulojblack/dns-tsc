0d08cc476f91ac732a0a1727ab33256701268f2f
Paulo Black <pblack@goodwaygroup.com>
2022-07-14 16:35:34 -0400

    refactor dns-tsc into oclif

--- src/commands/resolve.ts
+++ src/commands/resolve.ts
@@ -2,7 +2,7 @@ import * as dns from 'dns';
 import {Command, Flags} from '@oclif/core'
 
 export default class Resolve extends Command {
-  static description = 'Resolve a given DNS record (default A) for a URL'
+  static description = 'Resolve a given DNS record (default ANY) for a URL'
 
   static examples = [
       '<%= config.bin %> <%= command.id %>',
@@ -10,20 +10,23 @@ export default class Resolve extends Command {
 
   static flags = {
       nameServer: Flags.string({char: 'n', description: 'Name server to use'}),
+      resourceRecord: Flags.string({char: 'r', description: 'Resource record to request'})
   }
 
   static args = [
-      {name: 'domain', description: 'Domain to resolve', required: true},
-      {name: 'resourceRecord', description: 'DNS record', required: true},
+      {name: 'domain', description: 'Domain to resolve', required: true}
   ]
 
   public async run(): Promise<void> {
       const {args, flags} = await this.parse(Resolve)
-      console.log(args)
-      console.log(flags)
+      const { domain } = args;
+      const { resourceRecord, nameServer } = flags;
+      let result = await this.resolveDNS(domain, resourceRecord, nameServer)
+      this.log(result);
   }
 
-  private async resolveDNS(domain: string, recordType: string, nameServer: string): Promise<any> {
+  private async resolveDNS(domain: string, resourceRecord: string = 'ANY', nameServer: string = '8.8.8.8'): Promise<any> {
+    console.log(domain, resourceRecord, nameServer)
     const { Resolver } = dns.promises;
     const resolver = new Resolver()
     if (nameServer) {
@@ -33,7 +36,7 @@ export default class Resolve extends Command {
     let result
 
     try {
-      result = await resolver.resolve(domain, recordType)
+      result = await resolver.resolve(domain, resourceRecord)
     } catch (error: any) {
       throw new Error(error)
     }
