4f0fb643b590c655bb57d077dcbbb4edee31b523
Paulo Black <pblack@goodwaygroup.com>
2022-07-16 10:46:40 -0400

    feat(resolve): remove spinner from resolve logic; clean up cli

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
-exports = {}
-// https://www.npmjs.com/package/dns-packet
 const { Resolver } =  require('dns').promises
-const commander = require('commander')
-const ora = require('ora')
+const commander = require('commander');
 
 let resourceRecords: Array<string> = ['A', 'AAAA', 'ANY', 'CAA', 'CNAME', 'MX', 'NAPTR', 'NS', 'PTR', 'SOA', 'SRV', 'TXT'];
 interface CLIOpts {
@@ -18,12 +15,12 @@ function cli() {
         .version('0.1.0')
 
     program.command('resolve')
-        .description('Resolve a given DNS record (default A) for a URL')
+        .description('Resolve a given DNS record (default ANY) for a URL')
         .addArgument(new commander.Argument('[domain]', 'Domain to resolve'))
         .addArgument(new commander.Argument('[recordType]', 'DNS Record').choices(resourceRecords))
         .option('-n, --name-server <string>', 'Name server to request DNS')
         .action(async (domain: string, recordType: string, opts: CLIOpts): Promise<void> => {
-            let result = await resolveDNS(domain, recordType, opts);
+            let result = await resolveDNS(domain, recordType, opts.nameServer);
             console.log(result)
 
         })
@@ -31,18 +28,13 @@ function cli() {
     program.parse()
 }
 
-async function resolveDNS (domain: string, recordType: string, opts: CLIOpts) {
+async function resolveDNS (domain: string, recordType: string = 'ANY', nameServer: string = '8.8.8.8') {
     let resolver = new Resolver();
-    if (opts.nameServer) {
-        resolver.setServers([opts.nameServer]);
-    }
+    resolver.setServers([nameServer]);
+    
     let result;
-
     try {
-        const spinner = ora(`Resolving ${domain}`) // Produce ora CLI spinner while resolving DNS
-        spinner.start()
         result = await resolver.resolve(domain, recordType);
-        spinner.stop()
     } catch(e: any) {
         throw new Error(e)
     }
